caea6cdea5a1209c1b56a19baf121e9352dc67c8
saruulmn <enso.saruul@gmail.com>
2026-05-02 16:30:51 +0800

    update

--- app/[lang]/login/page.tsx
+++ app/[lang]/login/page.tsx
@@ -31,6 +31,7 @@ export default function LoginPage({ params }: { params: Promise<{ lang: Locale }
     }
   }, [status, lang, router]);
 
+  useEffect(() => {
     params.then((p) => {
       setLang(p.lang);
       getDictionary(p.lang).then((dict) => {
